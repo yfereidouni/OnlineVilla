83cbf1760160aa8994d0217a656c9ee8bb659edf
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-11-24 20:09:27 +0100

    GetVillas method was developed

--- OnlineVilla_API/Controllers/VillaAPIController.cs
+++ OnlineVilla_API/Controllers/VillaAPIController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OnlineVilla_API.Models;
 
 namespace OnlineVilla_API.Controllers;
 
 [ApiController]
 public class VillaAPIController : ControllerBase
 {
-
+    public IEnumerable<Villa> GetVillas()
+    {
+        return new List<Villa>
+        {
+            new Villa{Id =1, Name = "Pool View"},
+            new Villa{Id =2, Name = "Beach View"},
+        };
+    }
 }
