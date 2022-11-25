74d2459ece15f0242008dc2878bf534eff63854a
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-11-25 11:29:34 +0100

    StaticClass of ResponseCodes was added for more readability of ActionMethods

--- OnlineVilla_API/Controllers/VillaAPIController.cs
+++ OnlineVilla_API/Controllers/VillaAPIController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using OnlineVilla_API.Data;
 using OnlineVilla_API.Models;
 using OnlineVilla_API.Models.Dto;
@@ -11,12 +12,19 @@ namespace OnlineVilla_API.Controllers;
 public class VillaAPIController : ControllerBase
 {
     [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
     public ActionResult<IEnumerable<VillaDTO>> GetVillas()
     {
         return Ok(VillaStore.villaList);
     }
 
     [HttpGet("{id:int}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    //[ProducesResponseType(200, Type = typeof(VillaDTO))]
+    //[ProducesResponseType(400)]
+    //[ProducesResponseType(404)]
     public ActionResult<VillaDTO> GetVilla(int id)
     {
         if (id == 0)
